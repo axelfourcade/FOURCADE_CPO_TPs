f75977bda4ab335b798a120b419e40927cb4ad4b
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-04 16:09:40 +0200

    modification projet

--- TP1 exo1/src/tp1/exo1/TP1Exo1.java
+++ TP1 exo1/src/tp1/exo1/TP1Exo1.java
@@ -24,9 +24,33 @@ public class TP1Exo1 {
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Entrer la première valeur : ");
         valeur1 = sc.nextInt();
+        System.out.println("\n Entrer la seconde valeur : ");
         valeur2 = sc.nextInt();
         
-        system.out.println()
+        System.out.println("\nLa première valeur est : " + valeur1 + "\nLa seconde valeur est : " + valeur2);
+   
+        int somme = valeur1 + valeur2;
+        System.out.println("La somme de ces deux nombres est : " + somme);
+        
+        int difference = valeur1 - valeur2;
+        System.out.println("La différence entre ces deux nombres est : " + difference);
+
+        int produit = valeur1 * valeur2;
+        System.out.println("Le produit de ces deux nombres est : " + produit);
+
+        double quotient = valeur1 / (valeur2 * 1.0);
+        System.out.println("Le quotient de ces deux nombres est : " + quotient);
+ 
+        int reste = valeur1 % valeur2;
+        System.out.println("Le reste de la division euclidienne du premier par le second est : " + reste);
+
+        
+        
+        
+        
+        
+        
+    
     }
     
 }
