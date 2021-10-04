114f43a607f0603ff3beaa2b15a5f60f3e137263
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-04 16:36:43 +0200

    modification projet

---
+++ TP1 exo2/src/tp1/exo2/TP1Exo2.java
@@ -0,0 +1,21 @@
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE
+ * 04/10/2021
+ */
+package tp1.exo2;
+
+/**
+ *
+ * @author Axel
+ */
+public class TP1Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
