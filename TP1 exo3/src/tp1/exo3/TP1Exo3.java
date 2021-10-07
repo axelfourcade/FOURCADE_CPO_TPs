94445717bd436c6442708fe84a4915b5ad379331
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-07 10:15:59 +0200

    création projet

---
+++ TP1 exo3/src/tp1/exo3/TP1Exo3.java
@@ -0,0 +1,32 @@
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE
+ * 07/10/2021
+ */
+package tp1.exo3;
+
+/**
+ *
+ * @author Axel
+ */
+public class TP1Exo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
