a2b588a68e4bca861339926d617aa49327b79768
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-08 16:31:43 +0200

    modification projet

---
+++ TP1 4) Statistiques/src/tp1/pkg4/statistiques/TP14Statistiques.java
@@ -0,0 +1,29 @@
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE , Noa LEGIER
+ * 07/10/2021
+ */
+package tp1.pkg4.statistiques;
+
+/**
+ *
+ * @author Axel
+ */
+public class TP14Statistiques {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
