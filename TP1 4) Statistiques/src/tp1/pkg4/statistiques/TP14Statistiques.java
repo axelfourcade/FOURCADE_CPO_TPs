ed5e32cc6bff7aa6d4d98129624257e6c82b7080
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-08 16:51:43 +0200

    modification projet

--- TP1 4) Statistiques/src/tp1/pkg4/statistiques/TP14Statistiques.java
+++ TP1 4) Statistiques/src/tp1/pkg4/statistiques/TP14Statistiques.java
@@ -5,6 +5,9 @@
  */
 package tp1.pkg4.statistiques;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Axel
@@ -16,13 +19,30 @@ public class TP14Statistiques {
      */
     public static void main(String[] args) {
         
+    int tab[] = new int[6] ;
+   
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Saisir le nombre de lancé :");
+    int m = sc.nextInt();
+    int i = 0;
+    while (i<=m-1) {
+        Random r = new Random();
+        int n = r.nextInt(6);
+        tab[n]+= 1 ;
+        i+=1 ;
+    }
+    
+    double t[] = new double[6] ;
+    for (int a = 0 ; a<6;a++) {
+        t[a]= tab[a]*100/m ;
+    }
+    
+    for (i=1;i<7;i++){
         
-        
-        
-        
-        
-        
-        
+        System.out.println("Nous avons obtenu " + t[i-1] + "% de " + i);
+    }
+    
+   
         
     }
     
