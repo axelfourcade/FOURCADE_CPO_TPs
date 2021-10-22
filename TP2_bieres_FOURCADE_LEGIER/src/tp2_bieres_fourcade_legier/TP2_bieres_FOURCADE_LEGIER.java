bd14cbcd3bde38b52c231a2f54ef70bf7a841821
Axel <Axel@LAPTOP-8DC2K3BE>
2021-10-22 14:22:55 +0200

    création projet

---
+++ TP2_bieres_FOURCADE_LEGIER/src/tp2_bieres_fourcade_legier/TP2_bieres_FOURCADE_LEGIER.java
@@ -0,0 +1,21 @@
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE , Noa LEGIER
+ * 22/10/2021
+ */
+package tp2_bieres_fourcade_legier;
+
+/**
+ *
+ * @author Axel
+ */
+public class TP2_bieres_FOURCADE_LEGIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
