6364ec120f3e02f0ae1556e74675dae0da80d9c5
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-06 16:39:12 +0100

    exercice 2 debut

--- TP2_convertisseurObjet_FOURCADE_LEGIER/src/tp2_convertisseurobjet_fourcade_legier/Convertisseur.java
+++ TP2_convertisseurObjet_FOURCADE_LEGIER/src/tp2_convertisseurobjet_fourcade_legier/Convertisseur.java
@@ -1,6 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE , Noa LEGIER
+ * 06/11/2021
  */
 package tp2_convertisseurobjet_fourcade_legier;
 
@@ -9,15 +10,59 @@ package tp2_convertisseurobjet_fourcade_legier;
  * @author Axel
  */
 public class Convertisseur {
-    
-    
-    
+     
     int nbConversions;
     
+    public Convertisseur () {
+    nbConversions = 0 ;
+    }
     
     
+    public double CelciusVersKelvin ( double tempCelcius) {
+
+    double tempKelvin = tempCelcius + 273.15;
+    nbConversions += 1;
+    return tempKelvin;
+            }
+  
+    public double KelvinVersCelcius ( double tempKelvin) {
+
+    double tempCelcius = tempKelvin - 273.15;
+    nbConversions += 1;
+    return tempCelcius;
+            }
     
+    public double FarenheitVersCelcius ( double tempFar) {
+
+    double tempCelcius = 5.0 / 9 * ( tempFar - 32 );
+    nbConversions += 1;
+    return tempCelcius;
+            }
     
+    public double CelciusVersFarenheit ( double tempCelcius) {
+
+    double tempFar = 9.0 / 5 * tempCelcius + 32 ;
+    nbConversions += 1;
+    return tempFar;
+            }
     
+    public double  KelvinVersFarenheit ( double tempKelvin) {
+
+    double tempFar = 9.0 / 5 * tempKelvin - 459.67 ;
+    nbConversions += 1;
+    return tempFar;
+            }
     
+    public double  FarenheitVersKelvin ( double tempFar) {
+
+    double tempKelvin = 5.0 / 9 * ( tempFar + 459.67 );
+    nbConversions += 1;
+    return tempKelvin;
+            }
+    
+    @Override
+    public String toString () {
+    
+    return "nb de conversions : "+ nbConversions;
+    }
 }
--- TP2_convertisseurObjet_FOURCADE_LEGIER/src/tp2_convertisseurobjet_fourcade_legier/TP2_convertisseurObjet_FOURCADE_LEGIER.java
+++ TP2_convertisseurObjet_FOURCADE_LEGIER/src/tp2_convertisseurobjet_fourcade_legier/TP2_convertisseurObjet_FOURCADE_LEGIER.java
@@ -1,9 +1,11 @@
 /**
  * TP1 exercice 1
  * Axel FOURCADE , Noa LEGIER
- * 22/10/2021
+ * 06/11/2021
  */
 package tp2_convertisseurobjet_fourcade_legier;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +18,55 @@ public class TP2_convertisseurObjet_FOURCADE_LEGIER {
      */
     public static void main(String[] args) {
        
-    }
+        double valeur;
+                
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bonjour, saisissez une température : ");
+        valeur = sc.nextDouble();
+        
+        Convertisseur convertisseur = new Convertisseur();
+        
+        //convertisseur.CelciusVersKelvin(valeur);
+        //convertisseur.FarenheitVersKelvin(valeur);
+        //convertisseur.KelvinVersFarenheit(valeur);
+        
+        //Convertisseur convertisseur2 = new Convertisseur();
+
+        //convertisseur2.FarenheitVersCelcius(valeur);
+        //convertisseur2.CelciusVersFarenheit(valeur);
+        
+        //System.out.println(convertisseur.toString());
+        //System.out.println(convertisseur2.toString());
+
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :\n1) De Celcius vers Kelvin \n2) De Kelvin vers Celcius \n3) De Farenheit vers Celcius \n4) De Celcius vers Farenheit \n5) De kelvin vers Farenheit \n6) De Farenheit vers Kelvin");
+        int choix = sc.nextInt();
     
-}
+        if (choix == 1) {
+        System.out.println(valeur + " degré Celcius est égale à " + convertisseur.CelciusVersKelvin ( valeur) + " degré Kelvin");
+        }
+        else if (choix == 2){
+        System.out.println(valeur + " degré Kelvin est égale à " + convertisseur.KelvinVersCelcius ( valeur) + " degré Celcius");
+        }
+        else if (choix == 3){
+        System.out.println(valeur + " degré Farenheit est égale à " + convertisseur.FarenheitVersCelcius ( valeur) + " degré Celcius");
+        }
+        else if (choix == 4){
+        System.out.println(valeur + " degré Celcius est égale à " + convertisseur.CelciusVersFarenheit ( valeur) + " degré Farenheit");
+        }
+        else if (choix == 5){
+        System.out.println(valeur + " degré Kelvin est égale à " + convertisseur.KelvinVersFarenheit ( valeur) + " degré Farenheit");
+        }
+        else if (choix == 6){
+        System.out.println(valeur + " degré Farenheit est égale à " + convertisseur.FarenheitVersKelvin ( valeur) + " degré Kelvin");
+        }
+        else{
+        System.out.println("error");
+        }
+        }
+        
+        
+        
+        
+        
+        
+    }
