8660d4f5008c29d53d5c41707600f874be4c3ef1
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-11 00:36:09 +0100

    exercice 3 fini

---
+++ TP2_relation_1_FOURCADE_LEGIER/src/tp2_relation_1_fourcade_legier/TP2_relation_1_FOURCADE_LEGIER.java
@@ -0,0 +1,27 @@
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE , Noa LEGIER
+ * 10/11/2021
+ */
+package tp2_relation_1_fourcade_legier;
+
+/**
+ *
+ * @author Axel
+ */
+public class TP2_relation_1_FOURCADE_LEGIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+    
+}
---
+++ TP2_relation_1_FOURCADE_LEGIER/src/tp2_relation_1_fourcade_legier/Voiture.java
@@ -0,0 +1,14 @@
+package tp2_relation_1_fourcade_legier;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Axel
+ */
+public class Voiture {
+    
+}
