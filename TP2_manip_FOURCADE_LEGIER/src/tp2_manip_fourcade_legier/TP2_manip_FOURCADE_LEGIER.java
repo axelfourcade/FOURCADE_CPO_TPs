6e38851a89a945c810ef43573ddb38000e22bab6
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-10 23:38:06 +0100

    exercice 2 fini

---
+++ TP2_manip_FOURCADE_LEGIER/src/tp2_manip_fourcade_legier/TP2_manip_FOURCADE_LEGIER.java
@@ -0,0 +1,21 @@
+/**
+ * TP1 exercice 1
+ * Axel FOURCADE , Noa LEGIER
+ * 10/11/2021
+ */
+package tp2_manip_fourcade_legier;
+
+/**
+ *
+ * @author Axel
+ */
+public class TP2_manip_FOURCADE_LEGIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
