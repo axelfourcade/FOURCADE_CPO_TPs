b399c2c85ae08c8a9eee933616ee6fe7b120f3b9
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-12 18:51:49 +0100

    classe cellule debut

---
+++ SP4_console_FOURCADE_LEGIER/src/sp4_console_fourcade_legier/Cellule.java
@@ -0,0 +1,43 @@
+package sp4_console_fourcade_legier;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Axel
+ */
+public class Cellule {
+    
+Jeton jetonCourant;
+boolean trouNoir;
+boolean desintegrateur;
+
+public Cellule(){
+    jetonCourant = null ;
+    trouNoir = false ;
+    desintegrateur = false ;
+}
+    
+public boolean affecterJeton(Jeton jeton){
+    if (jetonCourant ==null){
+        Cellule Jeton;
+        return true ;
+    }
+    else {
+        return false;
+    }
+}    
+    
+public recupererJeton(){
+    
+}    
+    
+    
+    
+    
+    
+    
+}
