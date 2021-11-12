daf69a07fed5b58126ac53b230a967cd800d6aab
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-12 16:23:32 +0100

    creation class jeton

---
+++ SP4_console_FOURCADE_LEGIER/src/sp4_console_fourcade_legier/Jeton.java
@@ -0,0 +1,18 @@
+package sp4_console_fourcade_legier;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Axel
+ */
+public class Jeton {
+    
+    
+    
+    
+    
+}
---
+++ SP4_console_FOURCADE_LEGIER/src/sp4_console_fourcade_legier/SP4_console_FOURCADE_LEGIER.java
@@ -0,0 +1,29 @@
+/**
+ * TP3 console
+ * Axel FOURCADE , Noa LEGIER
+ * 12/11/2021
+ */
+package sp4_console_fourcade_legier;
+
+/**
+ *
+ * @author Axel
+ */
+public class SP4_console_FOURCADE_LEGIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+    }
+    
+}
