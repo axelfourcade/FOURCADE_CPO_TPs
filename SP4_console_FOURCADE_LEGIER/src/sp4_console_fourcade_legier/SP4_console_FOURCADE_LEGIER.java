cb719c93d0d7d297d098022512364266cbd55065
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-24 16:49:48 +0100

    version 4.0 fin

--- SP4_console_FOURCADE_LEGIER/src/sp4_console_fourcade_legier/SP4_console_FOURCADE_LEGIER.java
+++ SP4_console_FOURCADE_LEGIER/src/sp4_console_fourcade_legier/SP4_console_FOURCADE_LEGIER.java
@@ -18,30 +18,6 @@ public class SP4_console_FOURCADE_LEGIER {
      */
     public static void main(String[] args) {
 
-//Jeton j = new Jeton ("rouge");
-//System.out.println(j.lireCouleur());
-//Cellule c = new Cellule();
-//System.out.println(c.lireCouleurDuJeton());
-//c.affecterJeton(j);
-//System.out.println(c.lireCouleurDuJeton());
-//Grille g = new Grille();
-//g.afficherGrilleSurConsole();
-//g.ajouterJetonDansColonne(j,6);
-//g.afficherGrilleSurConsole();
-
-//Jeton j2 = new Jeton ("jaune");
-//g.ajouterJetonDansColonne(j2,1);
-//g.afficherGrilleSurConsole();
-
-//g.ajouterJetonDansColonne(j,6);
-//g.afficherGrilleSurConsole();
-
-//g.ajouterJetonDansColonne(j,6);
-//g.afficherGrilleSurConsole();
-
-//g.ajouterJetonDansColonne(j,6);
-
-//g.afficherGrilleSurConsole();
 
 Scanner sc = new Scanner(System.in);
 System.out.println("Joueur 1 quel est ton nom ?");
