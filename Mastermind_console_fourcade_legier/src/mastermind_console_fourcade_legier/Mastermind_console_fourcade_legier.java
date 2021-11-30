07e260279958d824b485e480df0d3b462179fa45
Axel <Axel@LAPTOP-8DC2K3BE>
2021-11-30 12:23:24 +0100

    mastermind creation

---
+++ Mastermind_console_fourcade_legier/src/mastermind_console_fourcade_legier/Mastermind_console_fourcade_legier.java
@@ -0,0 +1,30 @@
+/**
+ * TP4 console
+ * Axel FOURCADE , Noa LEGIER
+ * 30/11/2021
+ */
+package mastermind_console_fourcade_legier;
+
+/**
+ *
+ * @author Axel
+ */
+public class Mastermind_console_fourcade_legier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
---
+++ Mastermind_console_fourcade_legier/src/mastermind_console_fourcade_legier/Partie.java
@@ -0,0 +1,17 @@
+package mastermind_console_fourcade_legier;
+
+/**
+ * TP4 
+ * Axel FOURCADE , Noa LEGIER
+ * 12/11/2021
+ */
+
+/**
+ *
+ * @author Axel
+ */
+public class Partie {
+    
+    
+    
+}
